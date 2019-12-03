5a69b11fd0ffbbae8c62360b7c2a80260d043baa
unknown <ilyavusnya@gmail.com>
2019-12-03 21:05:46 +0200

    Added amazon logo on navbar and started content table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,36 @@
 @extends('layouts.app')
 @section('content')
-<div class="content full-height">
+<div id="bg-image">
 
+</div>
+<div class="container my-5">
+  <table class="table table-light text-center">
+    <thead class="thead-dark">
+      <th>#</th>
+      <th>Photo</th>
+      <th>Name</th>
+      <th>Price</th>
+      <th></th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>
+          <img src="https://images-na.ssl-images-amazon.com/images/I/71sBjbHYbKL._AC_SY200_.jpg"
+          alt="no-image"/>
+        </td>
+        <td>Headphones</td>
+        <td>15.00$</td>
+        <td>
+          <a href="#" class="btn btn-primary">
+            Add to Cart
+          </a>
+          <a href="#" class="btn btn-info">
+            More Info
+          </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 @endsection
