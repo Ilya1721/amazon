613df096d71e31fac9fbb73dbea11060a64c1a60
unknown <ilyavusnya@gmail.com>
2019-12-05 15:29:55 +0200

    Added item supplier country category models and migrations

---
+++ app/Item.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $guarded = [];
+
+    public function category()
+    {
+      return $this->belongsTo(Category::class);
+    }
+
+    public function supplier()
+    {
+      return $this->belongsTo(Supplier::class);
+    }
+}
---
+++ database/migrations/2019_12_05_130721_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->float('price');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('supplier_id');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
