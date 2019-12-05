04a594a1dc8c97745b7630431cbdfaba296a5285
unknown <ilyavusnya@gmail.com>
2019-12-05 16:01:39 +0200

    Connected data from database through controller to welcome page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Item;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-      return view('welcome');
+      $items = Item::all();
+
+      return view('welcome', [
+        'items' => $items,
+      ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,21 +7,20 @@
   <table class="table table-light text-center">
     <thead class="thead-dark">
       <th>#</th>
-      <th>Photo</th>
       <th>Name</th>
+      <th>Category</th>
       <th>Price</th>
+      <th>Supplier</th>
       <th></th>
     </thead>
     <tbody>
-      @for($i = 0; $i < 3; $i++)
+      @foreach($items as $item)
       <tr>
-        <td>1</td>
-        <td>
-          <img src="https://images-na.ssl-images-amazon.com/images/I/71sBjbHYbKL._AC_SY200_.jpg"
-          alt="no-image"/>
-        </td>
-        <td>Headphones</td>
-        <td>15.00$</td>
+        <td>{{ $item->id }}</td>
+        <td>{{ $item->name }}</td>
+        <td>{{ $item->category->name }}</td>
+        <td>{{ $item->price }}$</td>
+        <td>{{ $item->supplier->name }}</td>
         <td>
           <a href="#" class="btn btn-primary">
             Add to Cart
@@ -31,7 +30,7 @@
           </a>
         </td>
       </tr>
-      @endfor
+      @endforeach
     </tbody>
   </table>
 </div>
