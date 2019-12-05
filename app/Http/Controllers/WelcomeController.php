a567e193acd24f81ccdca154211ff59b9068fb85
unknown <ilyavusnya@gmail.com>
2019-12-05 16:35:30 +0200

    Added filter sort search forms to the welcome page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Item;
+use App\Category;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
       $items = Item::all();
+      $categories = Category::all();
 
       return view('welcome', [
         'items' => $items,
+        'categories' => $categories,
       ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,62 @@
 
 </div>
 <div class="container my-5">
+  <h2 class="text-center">All items available</h2>
+  <div class="row d-flex justify-content-center my-2">
+    <form action="/welcome/filter" method="GET" class="form-inline mr-3">
+      @csrf
+      <div class="input-group">
+        <select name="category" class="form-control">
+          @foreach($categories as $category)
+          <option value="{{ $category->id }}">
+            {{ $category->name }}
+          </option>
+          @endforeach
+        </select>
+        <div class="input-group-append">
+          <button class="btn btn-success" type="submit">
+            Filter<span class="glyphicon glyphicon-search"></span>
+          </button>
+        </div>
+      </div>
+    </form>
+    <form action="/welcome/search" method="GET"
+     class="form-inline w-25 mr-3">
+      @csrf
+      <div class="input-group">
+        <select name="category" class="form-control">
+          <option value="items.name">Name</option>
+        </select>
+        <input id="search" name="search"
+         class="form-control w-50 input-group-append"
+         type="text" placeholder="Search"
+         aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-success" type="submit">
+            Find<span class="glyphicon glyphicon-search"></span>
+          </button>
+        </div>
+      </div>
+    </form>
+    <form action="/welcome/sort" method="GET" class="form-inline">
+      @csrf
+      <div class="input-group">
+        <select name="rule" class="form-control w-25">
+          <option value="DESC">Descending</option>
+          <option value="ASC">Ascending</option>
+        </select>
+        <select name="category" class="form-control input-group-append w-25">
+          <option value="price">Price</option>
+          <option value="items.name">Alphabetically</option>
+        </select>
+        <div class="input-group-append">
+          <button class="btn btn-success" type="submit">
+            Sort<span class="glyphicon glyphicon-search"></span>
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
   <table class="table table-light text-center">
     <thead class="thead-dark">
       <th>#</th>
