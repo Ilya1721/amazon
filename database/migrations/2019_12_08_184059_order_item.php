48be2b58b41c04681d334ec82d209a42589225c0
unknown <ilyavusnya@gmail.com>
2019-12-08 20:42:25 +0200

    Create order model and order_item pivot table

---
+++ database/migrations/2019_12_08_184059_order_item.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OrderItem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('order_item', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->unsignedBigInteger('order_id');
+          $table->unsignedBigInteger('item_id');
+          $table->timestamp('created_at')->useCurrent();
+          $table->timestamp('updated_at')->useCurrent();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_item');
+    }
+}
