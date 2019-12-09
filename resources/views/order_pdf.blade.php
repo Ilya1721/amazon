38cca5b42615644fae5716a2161ee8221e2f6afa
unknown <ilyavusnya@gmail.com>
2019-12-09 21:53:12 +0200

    Fixed order_pdf view

--- resources/views/order_pdf.blade.php
+++ resources/views/order_pdf.blade.php
@@ -4,32 +4,29 @@
   <title>Order #{{ $order->id }}</title>
 </head>
 <body>
-  <div>
-    <div>
-      <h2>All orders</h2>
-      <table>
-        <thead>
-          <th>#</th>
-          <th>Phone number</th>
-          <th>State</th>
-          <th>City</th>
-          <th>Street</th>
-          <th>House</th>
-          <th>Flat</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{{ $order->id }}</td>
-            <td>{{ $order->phone_number}}</td>
-            <td>{{ $order->state }}</td>
-            <td>{{ $order->city }}</td>
-            <td>{{ $order->street }}</td>
-            <td>{{ $order->house }}</td>
-            <td>{{ $order->flat }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
+  <h2>All orders</h2>
+  <p></p>
+  <table>
+    <thead>
+      <th>#</th>
+      <th>Phone number</th>
+      <th>State</th>
+      <th>City</th>
+      <th>Street</th>
+      <th>House</th>
+      <th>Flat</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>{{ $order->id }}</td>
+        <td>{{ $order->phone_number}}</td>
+        <td>{{ $order->state }}</td>
+        <td>{{ $order->city }}</td>
+        <td>{{ $order->street }}</td>
+        <td>{{ $order->house }}</td>
+        <td>{{ $order->flat }}</td>
+      </tr>
+    </tbody>
+  </table>
 </body>
 </html>
